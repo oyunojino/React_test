3c41a8f953302b89aa888538904a6c2be983c485
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-21 19:30:12 +0900

    라우터가 지정되지 않은 모든 요청 처리

--- 02-simple-spa/src/App.js
+++ 02-simple-spa/src/App.js
@@ -3,6 +3,8 @@ import { Link, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Main from "./pages/Main";
+import Error404 from "./pages/Error404";
+import DepartmentGet from "./pages/DepartmentGet";
 function App() {
   return (
     <div>
@@ -23,6 +25,10 @@ function App() {
         <Route path="/about" element={<About />} />
         {/* 서브 라우팅 사용 */}
         <Route path="/main/*" element={<Main />} />
+        
+        {/* path 속성 없이 Route 지정
+              -> 지정되지 않은 모든 요청에 반응 */}
+        <Route path="/*" element={<Error404 />} />
       </Routes>
     </div>
   );
